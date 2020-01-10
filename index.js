199ba2e884ab980dd60d179410a1afb9b5b390c2
skuttenkuler <samkiddmusic@gmail.com>
2020-01-09 21:31:17 -0800

    inital inquire and sudo for retrun data

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ const team = {
     managers: {},
 }
 //initiate app
+init();
+function init(){
+    newRole();
+}
 // inquire role user is looking for
-
+function newRole(){
+    var roles = ["Engineer","Intern","Manager","--Done--"];
+    inquirer.prompt([
+        {
+            type: 'list',
+            message:"select new role",
+            name:'newRoles',
+            choices: roles,
+        }
+    ]).then(response => {
+        //if done render the HTML
+        console.log(response.data)
+    })
+}
 //ask for name, title, email with inquirer
