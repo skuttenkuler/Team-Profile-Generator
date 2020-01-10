8c67b7b9561626da16ab0ae8b413313f7bb2e02b
skuttenkuler <samkiddmusic@gmail.com>
2020-01-09 21:00:15 -0800

    test complete for Engineer

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,10 +1,22 @@
 const Engineer = require('../js/engineer.js');
 
 //what are we testing:
+describe("Engineer", () => {
     //when it initializion..
-    // get a github key and value
-    
+    describe("init", () => {
+        // get a github key and value
+        it("should get github key and value", () => {
+            var engineer = new Engineer("Sam", "Engineer", "me@me.com",1,"skuttenkuler")
+            expect(engineer.getGithub()).toEqual("skuttenkuler")
+        })
+    })
+    //getRole()
+    describe("getRole", () => {
+        it("should return Engineer string", () => {
+            var engineer = new Engineer("Sam", "Engineer", "me@me.com",1,"skuttenkuler")
+            expect(engineer.getRole()).toEqual("Engineer");
+        })
+    })
 
-    //test methods
-        //getGithub()
-        //getRole()
+})
+    
