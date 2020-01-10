8dd96f28a106cafee842c78bee82d016985806f6
skuttenkuler <samkiddmusic@gmail.com>
2020-01-09 21:47:08 -0800

    new inquirer strat

--- index.js
+++ index.js
@@ -24,13 +24,56 @@ function newRole(){
     inquirer.prompt([
         {
             type: 'list',
-            message:"select new role",
-            name:'newRoles',
+            name:'Role',
+            message:"Select new role..",
             choices: roles,
         }
     ]).then(response => {
         //if done render the HTML
-        console.log(response.data)
+        if(response.Role === "--Done--"){
+            renderHTML();
+        }
+        //else function employeeDetails with the role
+        else{
+            
+            employeeDetails(response.Role);
+        }
     })
 }
+function employeeDetails(role){
+    //make questions object
+    
+    //if Engineer push engineer question to questions object
+    //if Intern push intern question to questions object
+    //if Manager push manager question to questions object
+    inquirer.prompt([
+        {
+            type: 'prompt',
+            message:`What's the ${role}'s name?`,
+            name:'name',
+        },
+        {
+            type: 'prompt',
+            message:"What's the employee's title",
+            name:'name',
+        },
+        {
+            type: 'prompt',
+            message:"What's the employee's name",
+            name:'name',
+        }
+    ]).then(response => {
 //ask for name, title, email with inquirer
+
+//if Engineer role
+    //ask for github
+//if Intern 
+    //ask for school
+//if Manager
+    //ask for office number
+//take all responses and role
+//console.log data
+}
+function renderHTML(){
+    console.log("yo we full, no more hiring")
+}
